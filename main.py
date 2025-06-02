b050925073055444b5c68bbc21f78f91d2ba0357
Israel Hibraim <miarsito1@gmail.com>
2025-06-02 00:08:56 -0600

    Nuevo modelo, timegpt_long_horizon

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 from utils.data import get_ohlcv_dataframe, get_final_dataframe
 from utils.evaluator import evaluate_forecast
 from utils.plots import normal_plot, forecast_plot, forecast_only_future
-from models.foundation import timegpt
+from models.foundation import timegpt, timegpt_long
 st.set_page_config(layout="wide")
 
 token = st.selectbox("Token", ["BTC/USDT", "EUR/USDT"])
@@ -17,10 +17,10 @@ def load_final_df_cached(raw_df):
     return get_final_dataframe(raw_df)
     
 df = load_ohlcv_cached(token, "1h", "2010-01-01 00:00:00")
-st.dataframe(df.tail(3))
+st.dataframe(df.head(3))
 final_df = get_final_dataframe(df) 
 st.write("Datos procesados")
-st.dataframe(final_df.tail(3))
+st.dataframe(final_df.head(3))
 st.pyplot(normal_plot(final_df))
 
 MODELOS_DIR = "models" 
@@ -39,14 +39,25 @@ for fname in os.listdir(cat_path):
 
 modelo_sel = st.selectbox("Selecciona un modelo", sorted(modelos_disponibles))
 umbral = st.number_input("Umbral", 30)
-h = st.number_input("Horizonte", 24)
-if modelo_sel == "timegpt":
-    fcst_df = timegpt.forecast_model(final_df, h, "H")
-    st.write("Futuro horizon")
-    st.dataframe(fcst_df.tail(3))
-    st.pyplot(forecast_plot(final_df, fcst_df, ["TimeGPT"]), use_container_width=True)
-    st.write("Pronostico")
-    st.pyplot(forecast_only_future(final_df, fcst_df, h, ["TimeGPT"], umbral), use_container_width=True)
+st.write("Hora, Dia, Semana, Mes")
+h =  st.selectbox("Horizonte, hrs", [1, 24, 168, 730])
+
+match modelo_sel:
+    case "timegpt":
+        fcst_df = timegpt.forecast_model(final_df, h, "h")
+        st.write("Futuro horizon")
+        st.dataframe(fcst_df.head(3))
+        st.pyplot(forecast_plot(final_df, fcst_df, ["TimeGPT"]), use_container_width=True)
+        st.write("Pronostico")
+        st.pyplot(forecast_only_future(final_df, fcst_df, h, ["TimeGPT"], umbral), use_container_width=True)
+    case "timegpt_long":
+        fcst_df = timegpt_long.forecast_model(final_df, h, "h")
+        st.write("Futuro horizon")
+        st.dataframe(fcst_df.tail(3))
+        st.pyplot(forecast_plot(final_df, fcst_df, ["TimeGPT"]), use_container_width=True)
+        st.write("Pronostico")
+        st.pyplot(forecast_only_future(final_df, fcst_df, h, ["TimeGPT"], umbral), use_container_width=True)
+
 
 
 
