afe1baa8f6433b73f57129b9ab6c064e77b29b91
Israel Hibraim <miarsito1@gmail.com>
2025-06-03 02:09:23 -0600

    bug fixed

--- models/dl/auto_nhits.py
+++ models/dl/auto_nhits.py
@@ -2,24 +2,20 @@ from neuralforecast.auto import AutoNHITS
 from neuralforecast import NeuralForecast
 from utilsforecast.plotting import plot_series
 from neuralforecast.losses.pytorch import MAE
-from ray import tune
-def get_config(trial):
-    return {
-        "max_steps": 100,
-        "input_size": trial.suggest_categorical("input_size", [24, 48, 96]),
-        "learning_rate": trial.suggest_loguniform("learning_rate", 1e-4, 1e-2),
-        "batch_size": trial.suggest_categorical("batch_size", [32, 64, 128]),
-        "random_seed": 1,
-    }
+from neuralforecast.tsdataset import TimeSeriesDataset
+
+
 def forecast_model(train_df, h, num_sample, backend):
-    model = AutoNHITS(
+    forecast = AutoNHITS(
         h=h,
         loss=MAE(),
-        config=get_config,  
+        config=None,  #Con optuna, automaticamente busca los mejores hiperparametros
         backend=backend,
-        num_samples=num_sample
+        num_samples=num_sample,
+        gpus=0,
+        cpus=16
     )
-    nf = NeuralForecast(models=[model], freq="H")
+    nf = NeuralForecast(models=[forecast], freq="h")
     nf.fit(df=train_df)
-    Y_hat_df = nf.predict()
-    return Y_hat_df
+    y_hat = nf.predict()
+    return y_hat
